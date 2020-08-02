a66b5759669134c7216b20767812b83386c188f7
harmiksardar <harmik.sardar@gmail.com>
2020-08-03 00:30:30 +1000

    Updated config to run all tests

--- wdio.conf.js
+++ wdio.conf.js
@@ -52,8 +52,8 @@ exports.config = {
         // 5 instances get started at a time.
         maxInstances: 5,
         //
-        browserName: 'chrome',
-        acceptInsecureCerts: true
+          browserName: 'chrome',
+          acceptInsecureCerts: true
         // If outputDir is provided WebdriverIO can capture driver session logs
         // it is possible to configure which logTypes to include/exclude.
         // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
